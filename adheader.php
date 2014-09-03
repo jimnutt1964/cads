455ec9fcd7027b94dfb1f87707529d33284e8d1a
Jim <jim_nutt@hotmail.com>
2014-09-03 14:46:21 -0700

    remove links to cads site

--- adheader.php
+++ adheader.php
@@ -25,7 +25,7 @@ if ($vardebug) {echo "db file included<br>";}
 <script src='http://js.worthathousandwords.com/IA.jsh?pid=2532'></script>
 <script src="http://w.sharethis.com/button/buttons.js"></script><script>stLight.options({publisher:'74d1bc9a-0de5-47c3-9db1-a90f498256db'});</script><link href='http://www.classifiedads.com/search.rss?keywords=&amp;cid=18&amp;rid=7906' type='application/rss+xml' rel='alternate' title='ClassifiedAds.com RSS Feed'>
 
-<link href='http://www.classifiedads.com/search.rss?keywords=&amp;rid=7906' type='application/rss+xml' rel='alternate' title='ClassifiedAds.com RSS Feed'>
+<link href='' type='application/rss+xml' rel='alternate' title='ClassifiedAds.com RSS Feed'>
 
 <!-- Start of AnswerDash script -->
 <script>var AnswerDash;!function(e,t,n,s,a){if(!t.getElementById(s)){var i,r=t.createElement(n),c=t.getElementsByTagName(n)[0];e[a]||(i=e[a]=function(){i.__oninit.push(arguments)},i.__oninit=[]),r.type="text/javascript",r.async=!0,r.src="//p1.answerdash.com/answerdash.min.js?siteid=263",r.setAttribute("id",s),c.parentNode.insertBefore(r,c)}}(window,document,"script","answerdash-script","AnswerDash");</script>
--- mast.php
+++ mast.php
@@ -4,9 +4,9 @@
 	<div style="float:left;width:274px;height:44px;"><a  href="index.html" title="Classified Ads"><img src="images/logo.png" alt="Classified Ads" style="border:0;display:block;width:274px;height:40px;"></a>
 	</div>
 	<div class=titleend>
-		<a class=ceilingcat rel=nofollow href="http://www.classifiedads.com/login.php">Log in</a>
-		<a class=ceilingcat rel=nofollow href="http://www.classifiedads.com/info.php?help">Help</a>
-		<a class=ceilingcat rel=nofollow href="http://www.classifiedads.com/post.php">Post an Ad (free)</a>
+		<a class=ceilingcat rel=nofollow href="">Log in</a>
+		<a class=ceilingcat rel=nofollow href="">Help</a>
+		<a class=ceilingcat rel=nofollow href="">Post an Ad (free)</a>
 	</div>
 	<div class=clearer>
 	</div>
